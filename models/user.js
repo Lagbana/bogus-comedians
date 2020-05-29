165846b01387ed1219caa8f0f0a089480be7b275
jesseokeya <jesseokeya@gmail.com>
2020-05-29 13:39:02 -0400

    ichanges

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SnapshotSchema = new Schema(
+  {
+    domain: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    snapshot: {
+      type: String,
+      required: true,
+    },
+    title: String,
+    browser: String,
+    depth: {
+      type: Number,
+      required: true,
+    },
+    screenshot: {
+      required: true,
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Snapshot", SnapshotSchema);
